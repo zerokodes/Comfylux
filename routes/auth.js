35173173c67e452920a40faf3670472e85edf336
zerokodes <henrysam140@gmail.com>
2023-07-22 19:46:03 +0100

    implemented sign up and login logic

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,15 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const CryptoJS = require("crypto-js");
 //REGISTER
 router.post("/register",async (req,res) => {
     const newUser = new User ({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password,
+        password: CryptoJS.AES.encrypt(
+            req.body.password,
+            process.env.PASS_SEC
+            ).toString(),
     });
     try{
 
@@ -15,5 +19,27 @@ router.post("/register",async (req,res) => {
         res.status(500).json(err);
     }
     
+});
+
+//LOGIN
+router.post("/login", async(req, res) => {
+    try{
+        const user = await User.findOne({ username: req.body.username});
+        !user && res.status(401).json("Wrong credentials!")
+        const hashedPassword = CryptoJS.AES.decrypt(
+            user.password,
+            process.env.PASS_SEC
+        );
+        const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+        originalPassword !== req.body.password && res.status(401).json("Wrong credentials!");
+
+        //decouple data inorder to send other user details except password
+        const { password, ...others } = user._doc;
+
+        res.status(200).json(others);
+    } catch(err){
+        res.status(500).json(err)
+    }
 });
 module.exports = router
