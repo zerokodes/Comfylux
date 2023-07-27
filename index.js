a362f96e92cf46e56c431bc0274ccee2eebf020a
zerokodes <henrysam140@gmail.com>
2023-07-27 06:29:07 +0100

    added stats feature for user and implemented addProduct feature

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
 const app = express();
 
 dotenv.config();
@@ -17,7 +18,7 @@ mongoose
 app.use(express.json())
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
-
+app.use("/api/products", productRoute);
 app.listen(process.env.PORT || 5000, () => {
     console.log("Backend server is runnung")
 });
