487aa134e03078b7c5d98475dfd0da3a553ea179
Nduka Henry Chisom <henrysam140@gmail.com>
2023-09-18 05:05:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
-const stripeRoute = require("./routes/stripe");
+//const stripeRoute = require("./routes/stripe");
 const cors = require("cors");
 const app = express();
 
@@ -25,7 +25,8 @@ app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
-app.use("/api/checkout", stripeRoute);
+//implement this feature when frontend is ready
+//app.use("/api/checkout", stripeRoute);
 app.use(cors());
 app.listen(process.env.PORT || 5000, () => {
     console.log("Backend server is runnung")
